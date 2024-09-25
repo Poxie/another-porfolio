ceaf8c72eb1086966cb8827ce0c2bdb1682c3b1e
Poxie <poxen12@hotmail.com>
2024-09-25 23:47:58 +0200

    Add link prop to button component

--- components/button/index.tsx
+++ components/button/index.tsx
@@ -4,28 +4,44 @@ import DirectionHover from "../direction-hover";
 import { useRef } from "react";
 import useDirectionHover from "@/hooks/useDirectionHover";
 
-export default function Button({ children, onClick, disabled, className }: {
+export default function Button({ children, onClick, disabled, href, className }: {
     children: React.ReactNode;
     onClick?: () => void;
+    href?: string;
     disabled?: boolean;
     className?: string;
 }) {
-    const containerRef = useRef<HTMLButtonElement>(null);
+    const containerRef = useRef<any>(null);
     const hoverRef = useRef<HTMLDivElement>(null);
 
     useDirectionHover(containerRef, hoverRef);
-    return(
-        <button
-            className={twMerge(
-                "[--cut-size:15%] cut-corner py-2 px-4 flex items-center gap-3 text-primary border-[1px] border-secondary",
-                className,
-            )}
-            onClick={onClick}
-            disabled={disabled}
-            ref={containerRef}
-        >
-            {children}
 
+    const props = {
+        ref: containerRef,
+        onClick,
+        disabled,
+        className: twMerge(
+            "[--cut-size:15%] cut-corner py-2 px-4 flex items-center gap-3 text-primary border-[1px] border-secondary",
+            className,
+        ),
+    }
+    
+    if(href) {
+        return(
+            <a 
+                {...props}
+                href={href}
+                target="_blank"
+            >
+                {children}
+                <DirectionHover ref={hoverRef} />
+            </a>
+        )
+    }
+
+    return(
+        <button {...props}>
+            {children}
             <DirectionHover ref={hoverRef} />
         </button>
     )
