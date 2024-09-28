b8e2ebc3d4d253d95410fe0cfea52c75224afbd7
Poxie <poxen12@hotmail.com>
2024-09-29 01:31:47 +0200

    Add about summaries

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { ABOUT_TABS } from "@/constants/tabs";
 import { AboutTab } from "@/types";
 import { getAgeInYears } from "@/utils/dates";
+import { ABOUT_SUMMARIES } from "@/constants/about";
 
 export default function About() {
     const t = useTranslations('about');
@@ -18,9 +19,21 @@ export default function About() {
             </h2>
             <AboutTabs onChange={setActiveTab} />
 
-            <p className="mt-4 text-lg">
-                {t(`content.${activeTab}`, { age: getAgeInYears() })}
-            </p>
+            <div className="mt-4">
+                <p className="text-lg">
+                    {t(`content.${activeTab}.text`, { age: getAgeInYears() })}
+                </p>
+                <ul className="mt-6 flex gap-4">
+                    {ABOUT_SUMMARIES.about.map(summary => (
+                        <li 
+                            className="cut-corner flex-1 px-4 pr-10 py-3 text-lg text-nowrap border-[1px] border-secondary bg-secondary/30"
+                            key={summary}
+                        >
+                            {t(`content.${activeTab}.summary.${summary}`)}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </section>
     )
 }
---
+++ constants/about.ts
@@ -0,0 +1,3 @@
+export const ABOUT_SUMMARIES = {
+    about: ['start', 'fullstack', 'frontend', 'graduated'],
+}
