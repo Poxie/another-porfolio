bdd405768de8b28fb44aec797080d083fdc85428
Poxie <poxen12@hotmail.com>
2024-09-28 23:59:20 +0200

    Update useAnimateIntoView hook to accept any css properties to animate

--- hooks/useAnimateIntoView.ts
+++ hooks/useAnimateIntoView.ts
@@ -5,6 +5,10 @@ export const DEFAULT_INITIAL_STATE = {
     opacity: 0,
     transform: 'translateY(30px)',
 } as const;
+const DEFAULT_STATE = {
+    opacity: 1,
+    transform: 'translateY(0px)',
+} as const;
 const DEFAULT_DELAY = 0;
 const DEFAULT_DURATION = 600;
 const DEFAULT_THRESHOLD = .7;
@@ -13,17 +17,35 @@ export default function useAnimateIntoView(ref: RefObject<HTMLElement>, {
     duration=DEFAULT_DURATION, 
     threshold=DEFAULT_THRESHOLD, 
     initialState=DEFAULT_INITIAL_STATE,
+    state=DEFAULT_STATE,
     siblingRef,
 }: {
     delay?: number;
     duration?: number;
     threshold?: number;
+    state?: CSSProperties;
     initialState?: CSSProperties & {
         opacity: number;
     };
     siblingRef?: RefObject<HTMLElement>;
 } | undefined = {}) {
     const isVisible = useIsWithinScroll(ref, { siblingRef, threshold });
+
+    const addStyles = (styles: CSSProperties) => {
+        Object.entries(styles).forEach(([property, value]) => {
+            if(value === undefined) return;
+            ref.current!.style.setProperty(property, value.toString());
+        });
+    }
+    const addTransition = () => {
+        const properties = [...Object.keys(state), ...Object.keys(initialState)];
+
+        const uniqueProperties = [...new Set(properties)];
+        const transitionProperties = uniqueProperties.map(property => `${property} ${duration}ms`);
+
+        ref.current!.style.transition = transitionProperties.join(', ');
+        ref.current!.style.transitionDelay = `${delay}ms`;
+    }
 
     useEffect(() => {
         if(!ref.current) {
@@ -32,15 +54,12 @@ export default function useAnimateIntoView(ref: RefObject<HTMLElement>, {
         }
 
         if(!isVisible) {
-            ref.current.style.opacity = initialState.opacity.toString();
-            ref.current.style.transform = initialState.transform || '';
-            ref.current.style.transition = `opacity ${duration}ms, transform ${duration}ms`;
-            ref.current.style.transitionDelay = `${delay}ms`;
+            addStyles(initialState);
+            addTransition();
         }
 
         if(isVisible) {
-            ref.current.style.opacity = '1';
-            ref.current.style.transform = 'translateY(0px)';
+            addStyles(state);
         }
     }, [isVisible]);
 
