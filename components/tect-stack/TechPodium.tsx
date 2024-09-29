3db2f8d1f6cefd6102094f6403acb2e7efe5fb62
Poxie <poxen12@hotmail.com>
2024-09-29 02:50:37 +0200

    Animate podiums on scroll

--- components/tect-stack/TechPodium.tsx
+++ components/tect-stack/TechPodium.tsx
@@ -1,17 +1,48 @@
+"use client";
 import { TechLanguage } from "@/assets/json/types"
+import useAnimateIntoView from "@/hooks/useAnimateIntoView";
 import { useTranslations } from "next-intl";
+import { useRef } from "react";
 
 const DEFAULT_HEIGHT = 350;
-export default function TechPodium({ tech, heightDecimal }: {
+export default function TechPodium({ tech, index }: {
     tech: TechLanguage;
-    heightDecimal: number;
+    index: number;
 }) {
     const t = useTranslations('tech-stack');
 
-    const languageDescription = t(`${tech.id}.description`);
+    const isSecondHeight = [1,3].includes(index);
+    const isThirdHeight = [0,4].includes(index);
+
+    const ref = useRef<HTMLDivElement>(null);
+
+    let delay = 0;
+    if(isSecondHeight) delay = 300;
+    if(isThirdHeight) delay = 600;
+    
+    const { initialState } = useAnimateIntoView(ref, {
+        initialState: {
+            transform: 'translateY(100%)',
+        },
+        state: {
+            transform: 'translateY(0)',
+        },
+        threshold: 1,
+        duration: 600,
+        delay,
+    });
+
+    let heightDecimal = 1;
+    if(isSecondHeight) heightDecimal = 5/6;
+    if(isThirdHeight) heightDecimal = 4/6;
+    
     const podiumHeight = DEFAULT_HEIGHT * heightDecimal;
     return(
-        <div className="text-center">
+        <div 
+            style={initialState}
+            className="text-center"
+            ref={ref}
+        >
             <span className="mb-4 block text-4xl font-semibold">
                 {tech.language}
             </span>
@@ -20,7 +51,7 @@ export default function TechPodium({ tech, heightDecimal }: {
                 style={{ height: `${podiumHeight}px` }}
             >
                 <span className="p-4 block border-b-[1px] border-tertiary">
-                    {languageDescription}
+                    {t(`${tech.id}.description`)}
                 </span>
                 <ul className="p-4 text-secondary">
                     {tech.addons.map(addon => (
--- components/tect-stack/index.tsx
+++ components/tect-stack/index.tsx
@@ -3,20 +3,14 @@ import TechPodium from './TechPodium';
 
 export default function TechStack() {
     return(
-        <section className="main-width pt-section-base grid grid-cols-5 items-end" id="tech-stack">
-            {techStack.map((tech, index) => {
-                let heightDecimal = 1;
-                if([1,3].includes(index)) heightDecimal = 5/6;
-                if([0,4].includes(index)) heightDecimal = 4/6;
-
-                return(
-                    <TechPodium 
-                        tech={tech}
-                        heightDecimal={heightDecimal}
-                        key={tech.id}
-                    />
-                )
-            })}
+        <section className="main-width pt-section-base grid grid-cols-5 items-end overflow-hidden" id="tech-stack">
+            {techStack.map((tech, index) => (
+                <TechPodium 
+                    tech={tech}
+                    index={index}
+                    key={tech.id}
+                />
+            ))}
         </section>
     )
 }
